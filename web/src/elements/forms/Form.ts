e3f335afef41d0f8b484523ec861178714521906
Teffen Ellis <teffen@sister.software>
2025-07-09 12:44:24 +0200

    web: Clean up usage of method.

--- web/src/elements/forms/Form.ts
+++ web/src/elements/forms/Form.ts
@@ -232,9 +232,8 @@ export abstract class Form<T> extends AKElement {
 
     /**
      * Convert the elements of the form to JSON.[4]
-     *
      */
-    serializeForm(): T | undefined {
+    serialize(): T | undefined {
         const elements = this.shadowRoot?.querySelectorAll<HorizontalFormElement>(
             "ak-form-element-horizontal",
         );
@@ -252,7 +251,7 @@ export abstract class Form<T> extends AKElement {
     async submit(event: Event): Promise<unknown | undefined> {
         event.preventDefault();
 
-        const data = this.serializeForm();
+        const data = this.serialize();
         if (!data) return;
 
         return this.send(data)
